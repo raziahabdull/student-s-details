51b2634891e39119d8650f8bd33e3e222de6ac1f
raziah <abdullahiraziah@gmail.com>
2024-02-07 08:22:51 +0300

    version 1

---
+++ Main.kt
@@ -0,0 +1,17 @@
+fun main(){
+    var  fullName = "Raziah Abdullahi"
+    println(fullName)
+     var age = 20
+    println(age)
+
+    var weightInKg = 54
+    println(weightInKg)
+
+    var KenyanCitizen = "True"
+    println(KenyanCitizen)
+
+    var phoneNumber = "0711143646"
+    println(phoneNumber)
+
+}
+
